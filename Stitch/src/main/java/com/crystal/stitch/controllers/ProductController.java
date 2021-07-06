f319f99d42d709b9668b10cd246df60ee700c112
Adan Lara <adanlara8@gmail.com>
2021-07-05 18:10:46 -0700

    catalog cart redirect

--- Stitch/src/main/java/com/crystal/stitch/controllers/ProductController.java
+++ Stitch/src/main/java/com/crystal/stitch/controllers/ProductController.java
@@ -1,21 +1,40 @@
 package com.crystal.stitch.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.crystal.stitch.models.User;
+import com.crystal.stitch.services.CartService;
 import com.crystal.stitch.services.ProductService;
+import com.crystal.stitch.services.UserService;
 
 @Controller
 public class ProductController {
 	@Autowired 
 	private ProductService pService;
-
+	@Autowired
+	private UserService uServ;
+	@ Autowired 
+	private CartService cartServ;
 
 	@GetMapping("/{categories}")
-	public String products(@PathVariable("categories") String categories, Model viewModel) {
+	public String products(@PathVariable("categories") String categories, Model viewModel, HttpSession session) {
+		
+		// needed for navbar tag <c:choose>
+		Long currentUserId = (Long)session.getAttribute("theUserId");
+		User currentUser= this.uServ.findUserById(currentUserId);
+		viewModel.addAttribute("guest",currentUser);
+		
+		viewModel.addAttribute("loginUser",currentUser);
+		Long currentCartId = (Long) session.getAttribute("cart__id");
+		viewModel.addAttribute("cart",this.cartServ.findCartbyId(currentCartId));				
+
+		
 		if(categories.equals("mJackets")){
 			viewModel.addAttribute("product", this.pService.getGenderAndItem("mens", "jacket"));
 			}
