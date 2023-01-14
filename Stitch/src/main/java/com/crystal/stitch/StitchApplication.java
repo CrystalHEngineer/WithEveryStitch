0342c1630a3d0d5d0829e7035130cab39767bad6
Crystal Harper <crystalhaylen@gmail.com>
2023-01-14 12:21:30 -0600

    Trying it out

--- Stitch/src/main/java/com/crystal/stitch/StitchApplication.java
+++ Stitch/src/main/java/com/crystal/stitch/StitchApplication.java
@@ -7,22 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
 public class StitchApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(StitchApplication.class, args);
-    }
-    @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.setPort(9090);
-        ajpConnector.setSecure(false);
-        ajpConnector.setAllowTrace(false);
-        ajpConnector.setScheme("http");
-       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
-       tomcat.addAdditionalTomcatConnectors(ajpConnector);
-    return tomcat;
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StitchApplication.class, args);
+	}
+
 }
